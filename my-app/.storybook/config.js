c4871982b4f123be1256f5f16983022445624918
verusz <523726352@qq.com>
2019-11-05 15:37:24 -0500

    improve the UI version of webpage

--- my-app/.storybook/config.js
+++ my-app/.storybook/config.js
@@ -2,3 +2,13 @@ import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+// import { configure } from '@storybook/react';
+// import '../src/index.css';
+//
+// const req = require.context('../src', true, /\.stories.js$/);
+//
+// function loadStories() {
+//     req.keys().forEach(filename => req(filename));
+// }
+//
+// configure(loadStories, module);
